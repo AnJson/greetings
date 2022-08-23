6c87fb3da040ca9ce659f99dd9a710eff294883f
Anders Jonsson <aj224rj@student.lnu.se>
2022-08-23 12:56:04 +0200

    Add submit-handler to form

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,25 @@
  */
 import React from 'react'
 import Header from './components/layout/Header'
+import Output from './components/Output'
 import TextinputForm from './components/TextinputForm'
 
 import classes from './App.module.css'
-import Output from './components/Output'
 
+/**
+ * Main component for the application.
+ */
 function App() {
+  // NOTE: Add jsDoc.
+  const submitHandler = (input: string) => {
+    console.log(input)
+  }
   return (
     <div className={classes.wrapper}>
       <Header />
       <div className={classes.main}>
         <Output text='What is your name?' />
-        <TextinputForm />
+        <TextinputForm onSubmit={submitHandler}/>
       </div>
     </div>
   )
--- src/components/TextinputForm.tsx
+++ src/components/TextinputForm.tsx
@@ -1,23 +1,41 @@
-import React, { FormEvent } from 'react'
+import React, { FormEvent, useRef } from 'react'
 
 import classes from './TextinputForm.module.css'
+
+// Define prop-types.
+type Props = {
+  onSubmit: (input: string) => void
+} 
 
 /**
  * Form-component for submitting answers.
  *
  * @returns {JSX} - JSX.
  */
-const TextinputForm = () => {
-  // NOTE: Temporary submithandler.
+const TextinputForm = ({ onSubmit }: Props) => {
+  const textInput = useRef<HTMLInputElement>(null)
+
+  // Prevent default behaviour on submit, validate and trigger onSubmit-function.
   const submitHandler = (event: FormEvent) => {
     event.preventDefault()
-    console.log('submitted')
+
+    if (textInput.current) {
+      const value: string = textInput.current.value
+
+      if (value.trim() !== '') {
+        onSubmit(textInput.current.value)
+      }
+
+      textInput.current.value = ''
+    }
+
+    
   }
 
   return (
     <div className={classes.wrapper}>
       <form className={classes.form} onSubmit={submitHandler}>
-        <input className={classes['text-input']} type='text' />
+        <input ref={textInput} className={classes['text-input']} type='text' />
         <button className={classes['submit-button']}>Send</button>
       </form>
     </div>
